70da6691233590f9ec8590667397a724678f4525
Giuseppe Randazzo <giupeppolo21@gmail.com>
2025-12-01 17:25:15 +0100

    feat: add navbar and page components for home, favorites, and show details

---
+++ src/app/pages/favorites_page/favorites_page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorites-page.component.html',
+  styleUrl: './favorites-page.component.scss',
+})
+export class FavoritesPageComponent {}
---
+++ src/app/pages/home_page/home_page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './home_page.component.html',
+  styleUrl: './home_page.component.scss',
+})
+export class HomePageComponent {}
